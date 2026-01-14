ae218ab81914152696831e46f3159d7b15fcd079
YashmaanSingh <156333050+YashmaanSingh@users.noreply.github.com>
2026-01-14 18:42:17 +0530

    refactor: remove Progress mongoose model

--- digital-learning-project/backend/models/Progress.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const progressSchema = new mongoose.Schema({
-  studentId: mongoose.Types.ObjectId,
-  lessonId: mongoose.Types.ObjectId,
-  completed: Boolean
-});
-
-module.exports = mongoose.model('Progress', progressSchema);
