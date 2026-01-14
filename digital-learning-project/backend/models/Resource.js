19a73b9decbfd6ed3c8b8986c7723bbdcf2d71f0
YashmaanSingh <156333050+YashmaanSingh@users.noreply.github.com>
2026-01-14 18:43:16 +0530

    refactor: remove Resource mongoose model

--- digital-learning-project/backend/models/Resource.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const resourceSchema = new mongoose.Schema({
-  title: String,
-  url: String,
-  uploadedBy: mongoose.Types.ObjectId
-});
-
-module.exports = mongoose.model('Resource', resourceSchema);
