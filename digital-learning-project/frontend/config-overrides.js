56e5c376a0afd30bf21ef99c7ad940da8b6321f2
YashmaanSingh <156333050+YashmaanSingh@users.noreply.github.com>
2026-01-19 12:27:13 +0530

    clean up webpack resolve.fallback duplicates

--- digital-learning-project/frontend/config-overrides.js
+++ digital-learning-project/frontend/config-overrides.js
@@ -2,13 +2,13 @@ const webpack = require("webpack");
 
 module.exports = function override(config) {
   config.resolve.fallback = {
-    url: require.resolve("url/"),
+    url: require.resolve("url"),
     path: require.resolve("path-browserify"),
     stream: require.resolve("stream-browserify"),
     crypto: require.resolve("crypto-browserify"),
-    assert: require.resolve("assert/"),
+    assert: require.resolve("assert"),
     process: require.resolve("process/browser"),
-    buffer: require.resolve("buffer/")
+    buffer: require.resolve("buffer")
   };
 
   config.plugins.push(
